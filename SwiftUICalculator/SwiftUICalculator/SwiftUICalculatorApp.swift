e9bf605025a77f078600a87e3f88c5ee1b77849a
Taranpreet Singh <52269769+TARANPREETS1999@users.noreply.github.com>
2023-10-25 19:08:15 +0530

    Add files via upload

---
+++ SwiftUICalculator/SwiftUICalculator/SwiftUICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICalculatorApp.swift
+//  SwiftUICalculator
+//
+//  Created by Taranpreet Singh on 27/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
